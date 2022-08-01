d7bc781f76b9df24b257f54b3d96edde3dc60ac7
BrayanGonzalez0105 <104708317+BrayanGonzalez0105@users.noreply.github.com>
2022-07-31 22:51:41 -0500

    Add files via upload

---
+++ Practica_3/gr-Modulos_J1C_02/build/docs/doxygen/html/api_8h.js
@@ -0,0 +1,4 @@
+var api_8h =
+[
+    [ "MODULOS_J1C_02_API", "api_8h.html#a2e79c4790cfe798968256570f6f04382", null ]
+];
---
+++ Practica_3/gr-Modulos_J1C_02/build/docs/doxygen/html/modules.js
@@ -0,0 +1,4 @@
+var modules =
+[
+    [ "GNU Radio MODULOS_J1C_02 C++ Signal Processing Blocks", "group__block.html", null ]
+];
