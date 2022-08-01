32fcb3ef4961eb6e02f4e60a8e068f8758283723
BrayanGonzalez0105 <104708317+BrayanGonzalez0105@users.noreply.github.com>
2022-07-31 22:50:31 -0500

    Add files via upload

---
+++ Practica_3/gr-Modulos_J1C_02/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/labcom/gr-Modulos_J1C_02
+# Build directory: /home/labcom/gr-Modulos_J1C_02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("include/Modulos_J1C_02")
+subdirs("lib")
+subdirs("apps")
+subdirs("docs")
+subdirs("python")
+subdirs("grc")
---
+++ Practica_3/gr-Modulos_J1C_02/build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/labcom/gr-Modulos_J1C_02/apps
+# Build directory: /home/labcom/gr-Modulos_J1C_02/build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
